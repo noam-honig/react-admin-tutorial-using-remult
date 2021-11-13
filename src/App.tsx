9718212b89dde7307abead531258a7891a35b5ee
Noam <noam@fireflymigration.com>
2021-11-13 08:31:47 +0200

    Adding search and filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import * as React from "react";
 import { Admin, ListGuesser, Resource } from 'react-admin';
 import { remult } from "./common";
-import { PostList } from "./components/Posts";
+import { PostCreate, PostEdit, PostList } from "./components/Posts";
 import { UserList } from "./components/UserList";
 import { RemultReactAdminDataProvider } from "./dev/remultReactAdminDataProvider";
 import { Post } from "./model/Post";
 import { User } from "./model/User";
 
 
-const dataProvider = new RemultReactAdminDataProvider(remult, [User,Post]);
+const dataProvider = new RemultReactAdminDataProvider(remult, [User, Post]);
 const App = () => <Admin dataProvider={dataProvider} >
   <Resource name="users" list={UserList} />
-  <Resource name="posts" list={PostList} />
+  <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
 </Admin>;
 
 export default App;
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import { List, Datagrid, TextField, ReferenceField } from 'react-admin';
+import { List, Datagrid, TextField, ReferenceField, Edit, SimpleForm, ReferenceInput, SelectInput, TextInput, Create } from 'react-admin';
 
 export const PostList = (props: any) => (
-    <List {...props}>
-        <Datagrid rowClick="edit">
+    <List {...props} filters={postFilters}>
+        <Datagrid rowClick="edit" >
             <ReferenceField source="userId" reference="users">
                 <TextField source="name" />
             </ReferenceField>
@@ -11,3 +11,39 @@ export const PostList = (props: any) => (
         </Datagrid>
     </List>
 );
+
+export const PostEdit = (props: any) => (
+    <Edit {...props} title={<PostTitle />}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = (props: any) => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Create>
+);
+
+const PostTitle = (props: any) => {
+    const { record } = props;
+
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+const postFilters = [
+    <TextInput source="title.$contains" label="Search" alwaysOn />,
+    <ReferenceInput source="userId" label="User" reference="users" allowEmpty>
+        <SelectInput optionText="name" />
+    </ReferenceInput>,
+];
