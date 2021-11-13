dff61a88dfcef33a365b0950fcb48e8511a95949
Noam <noam@fireflymigration.com>
2021-11-13 08:12:04 +0200

    customizing styles

--- src/components/MyUrlField.tsx
+++ src/components/MyUrlField.tsx
@@ -1,13 +1,27 @@
 // in src/MyUrlField.js
 import * as React from "react";
 import { useRecordContext } from 'react-admin';
+import { makeStyles } from '@material-ui/core/styles';
+import LaunchIcon from '@material-ui/icons/Launch';
+const useStyles = makeStyles({
+    link: {
+        textDecoration: 'none',
+    },
+    icon: {
+        width: '0.5em',
+        height: '0.5em',
+        paddingLeft: 2,
+    },
+});
 
 const MyUrlField = (args: any) => {
     let { source } = args;
     const record = useRecordContext();
+    const classes = useStyles();
     return record ? (
-        <a href={record[source]}>
+        <a href={record[source]} className={classes.link} >
             {record[source]}
+            <LaunchIcon className={classes.icon} />
         </a>
     ) : null;
 }
