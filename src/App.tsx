f6ae7486eb452d63df406193b0ea3583a8d84525
Noam <noam@fireflymigration.com>
2021-11-13 08:39:32 +0200

    Adding a Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,12 @@ import { User } from "./model/User";
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import Dashboard from "./components/Dashboard";
+import authProvider from "./components/authProvider";
 
 
 const dataProvider = new RemultReactAdminDataProvider(remult, [User, Post]);
 const App = () =>
-  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard} authProvider={authProvider}>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>;
---
+++ src/components/authProvider.ts
@@ -0,0 +1,31 @@
+export default {
+    // called when the user attempts to log in
+    login: (args: any) => {
+        const { username } = args;
+        localStorage.setItem('username', username);
+        // accept all username/password combinations
+        return Promise.resolve();
+    },
+    // called when the user clicks on the logout button
+    logout: () => {
+        localStorage.removeItem('username');
+        return Promise.resolve();
+    },
+    // called when the API returns an error
+    checkError: (args: any) => {
+        const { status } = args;
+        if (status === 401 || status === 403) {
+            localStorage.removeItem('username');
+            return Promise.reject();
+        }
+        return Promise.resolve();
+    },
+    // called when the user navigates to a new location, to check for authentication
+    checkAuth: () => {
+        return localStorage.getItem('username')
+            ? Promise.resolve()
+            : Promise.reject();
+    },
+    // called when the user navigates to a new location, to check for permissions / roles
+    getPermissions: () => Promise.resolve(),
+};
